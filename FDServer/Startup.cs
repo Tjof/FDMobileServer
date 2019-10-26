c398fda7b11e3d959d3ce678195039eb4351d979
PIBOP0116001 Тимченко Дмитрий Сергеевич <PIBOP0116001@sibupk.su>
2019-10-26 22:48:44 +0700

    Выводит что-то

--- FDServer/Startup.cs
+++ FDServer/Startup.cs
@@ -35,7 +35,21 @@ namespace FDServer
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
